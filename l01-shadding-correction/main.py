2b3b38abf0698b6a9c4e67b6a4beb8f8cadd592a
tiago-rodrigues1 <tiagords127@gmail.com>
2025-07-24 11:40:43 -0300

    fix: change path

--- l01-shadding-correction/main.py
+++ l01-shadding-correction/main.py
@@ -1,8 +1,8 @@
 import cv2 as cv
 import numpy as np
 
-img = cv.imread('lec_01/tungsten_filament_shaded.tif').astype(np.float32)
-shade = cv.imread('lec_01/tungsten_sensor_shading.tif').astype(np.float32)
+img = cv.imread('l01-shadding-correction/tungsten_filament_shaded.tif').astype(np.float32)
+shade = cv.imread('l01-shadding-correction/tungsten_sensor_shading.tif').astype(np.float32)
 
 # The images have the same size, so resizing is not necessary
 
@@ -13,4 +13,4 @@ result = img / shade
 
 normalized = np.clip(result * 255.0, 0, 255).astype(np.uint8)
 
-cv.imwrite('lec_01/result.tif', normalized)
+cv.imwrite('l01-shadding-correction/result.tif', normalized)
