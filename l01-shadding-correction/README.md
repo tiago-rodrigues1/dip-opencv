675cf87e2df73a39510babde35439562c5a931f7
tiago-rodrigues1 <tiagords127@gmail.com>
2025-07-24 00:44:05 -0300

    add(l01): shadding correction

---
+++ l01-shadding-correction/README.md
@@ -0,0 +1,3 @@
+# Shadding Correction
+
+The program performs shading correction on the tungsten_filament_shaded.tif image using pixel-wise division with a reference image that contains the shading pattern. This compensates for uneven illumination and restores uniform brightness.
---
+++ l01-shadding-correction/main.py
@@ -0,0 +1,16 @@
+import cv2 as cv
+import numpy as np
+
+img = cv.imread('lec_01/tungsten_filament_shaded.tif').astype(np.float32)
+shade = cv.imread('lec_01/tungsten_sensor_shading.tif').astype(np.float32)
+
+# The images have the same size, so resizing is not necessary
+
+# Remove 0's from shade
+shade[shade == 0] = 1e-5
+
+result = img / shade
+
+normalized = np.clip(result * 255.0, 0, 255).astype(np.uint8)
+
+cv.imwrite('lec_01/result.tif', normalized)
